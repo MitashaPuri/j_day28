fb0aec160cc7b55a11b502cf33fb5a6b9f95d0b6
MitashaPuri <99736676+MitashaPuri@users.noreply.github.com>
2025-01-28 11:14:31 +0530

    Add files via upload

---
+++ 28_jan.js
@@ -0,0 +1,35 @@
+//imp ques
+//  How many ways an HTML element can be accessed in JavaScript code? 
+// There are four possible ways to access HTML elements in JavaScript which are?
+// getElementById() Method: It is used to get the element by its id name.
+// getElementsByClass() Method: It is used to get all the elements that have the given classname.
+// getElementsByTagName() Method: It is used to get all the elements that have the given tag name.
+// querySelector() Method: This function takes CSS style selector and returns the first selected element.
+
+// 1. function
+// function (para1,para2){
+
+// }
+// function(arg1,arg2);
+// Function Return
+// When JavaScript reaches a return statement, the function will stop executing.
+
+
+//2. Objects
+// JavaScript variables are containers for data values.
+// This code assigns a simple value (Fiat) to a variable named car:
+// const car = {type:"Fiat", model:"500", color:"white"};
+
+// to define
+// Using an Object Literal
+// Using the new Keyword
+// Using an Object Constructor
+// JavaScript Objects are Mutable
+// Objects are mutable: They are addressed by reference, not by value.
+// new Object()   // A new Object object
+// new Array()    // A new Array object
+// new Map()      // A new Map object
+// new Set()      // A new Set object
+// new Date()     // A new Date object
+// new RegExp()   // A new RegExp object
+// new Function() // A new Function object
